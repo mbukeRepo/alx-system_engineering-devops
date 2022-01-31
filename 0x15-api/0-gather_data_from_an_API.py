a0da801caeb3382c5e1ba507c4f402d3a1f6ae0e
mbukeRepo <mbuke404@gmail.com>
2022-01-31 16:25:12 +0200

    reconfigured the code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-""" fetches users with respective todos
-    @param:
-       employeeID: int , represents the user id
-"""
+""" fetches todos """
 
 import requests
 import sys
@@ -11,17 +8,10 @@ if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
     todos = requests.get(base_url + "/users/{}/todos"
                          .format(sys.argv[1])).json()
-    done = {}
-    
-    for todo in todos:
-        done.setdefault(todo.get("userId"), [])
-        if todo.get("completed"):
-            done.get(todo.get("userId")).append(todo.get("title"))
-
-    for id, completed in done.items():
-        username = requests.get(base_url + "/users/{}"
-                                .format(id)).json()["name"]
-        print("Employee {} is done with tasks({}/20):"
-              .format(username, len(completed)))
-        for c in completed:
-            print("    {}".format(c))
+    username = requests.get(base_url + "/users/{}"
+                            .format(sys.argv[1])).json().get("name")
+    print(username)
+    completed = [todo.get("title") for todo in todos if todo.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(
+        username, len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
