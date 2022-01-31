2684ca5be88f3d06262158acefb231b930fd6cc5
mbukeRepo <mbuke404@gmail.com>
2022-01-31 14:48:11 +0200

    consumed /todos and /users endpoint

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" fetches users with respective todos
+    @param:
+       employeeID: int , represents the user id
+"""
+
+import requests
+
+base_url = "https://jsonplaceholder.typicode.com"
+todos = requests.get(base_url + "/todos").json()
+done = {}
+
+for todo in todos:
+    done.setdefault(todo["userId"], [])
+    done[todo["userId"]].append(todo["title"])
+
+for id, completed in done.items():
+    username = requests.get(base_url + "/users/{}".format(id)).json()["name"]
+    print("Employee {} is done with tasks({}/20):"
+          .format(username, len(completed)))
+    for c in completed:
+        print("    {}".format(c))
